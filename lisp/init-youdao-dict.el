fad09a96fc886457c0257bf21ca1901e65a3c807
Bliss Chung <bliss@3zso.com>
2014-03-17 01:39:49 +0800

    update zdict

--- lisp/init-youdao-dict.el
+++ lisp/init-youdao-dict.el
@@ -9,7 +9,11 @@
 (defun yodao-dict-search-wordap (&optional word)
   "WORD Use python script dict to look up word under point."
   (interactive)
-  (or word (setq word (current-word)))
+  (or word (setq word (if (use-region-p)
+               (buffer-substring-no-properties
+                (region-beginning) (region-end))
+             (current-word))))
+  (message word)
   (tooltip-show
    (shell-command-to-string
     (format "python ~/gnu/plugins/dict/zdict.py %s %s" word "False"))))
