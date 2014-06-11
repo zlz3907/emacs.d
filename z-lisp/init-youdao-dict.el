9d3e543339c53a243e6b73a052aaa2d1e47521fd
Zhong Lizhi <zhonglizhi@8chedao.com>
2014-06-11 15:40:56 +0800

    update

--- z-lisp/init-youdao-dict.el
+++ z-lisp/init-youdao-dict.el
@@ -1,11 +1,10 @@
 ;;; init-youdao-dict.el --- Dict
 
 ;;; Commentary:
+;; Youdao dict
 ;;
 
 ;;; Code:
-
-;; Youdao dict
 (defun yodao-dict-search-wordap (&optional word)
   "WORD Use python script dict to look up word under point."
   (interactive)
@@ -18,7 +17,7 @@
    (shell-command-to-string
     (format "python ~/gnu/plugins/dict/zdict.py '%s' %s" word "False"))))
 
-(global-set-key [f6] 'yodao-dict-search-wordap)
+(global-set-key [f7] 'yodao-dict-search-wordap)
 
 (provide 'init-youdao-dict)
 
