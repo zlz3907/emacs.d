75646954f682f8a7711b00fd73f1963c39f77862
Zhong Lizhi <zhonglizhi@8chedao.com>
2014-06-25 10:07:54 +0800

    ignore server

--- custom.el
+++ custom.el
@@ -35,10 +35,10 @@
 
 (require 'init-qiang-set-font)
 (qiang-set-font
-;; '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New")
- '("Ubuntu Mono" "Monospace" "Tahama" "Verdana" "Courier New")
+ '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New")
+ ;;'("YaHei Consolas Hybrid")
  ":pixelsize=16"
- '("YaHei Consolas Hybrid" "Microsoft Yahei" "黑体" "新宋体" "宋体"))
+ '("YaHei Consolas Hybrid"))
 
 ;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'load-path "~/gnu/plugins/org-mode/lisp")
@@ -72,13 +72,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(jde-ant-home "~/apache/ant")
+ '(jde-sourcepath (quote ("~/java/jdk1.8.0_05/src.zip" "~/.ivy2/*/*/*/*/sources/*.jar")))
+ '(large-file-warning-threshold 60000000)
+ '(org-agenda-files (quote ("~/org/2014")))
+ '(org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+ '(semantic-idle-scheduler-idle-time 3600)
+ '(semanticdb-default-save-directory "~/tmp/semanticdb")
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(menu ((t (:width normal :family "DejaVu Sans Mono")))))
 
 ;;; custom.el ends here
