df0462c2183094dd057fe4824a886cf14a438552
Bliss Chung <bliss@3zso.com>
2014-12-16 23:54:18 +0800

    添加offlineimap插件，集成offlineimap到emacs里

--- z-lisp/init-gnus.el
+++ z-lisp/init-gnus.el
@@ -62,6 +62,48 @@
 
 (require 'smtpmail)
 
+
+;; python api
+(require 'netrc)
+(defun netrc-account-machine (list machine account &optional port defaultport)
+  "Return the netrc values from LIST for MACHINE or for the default entry.
+       If PORT specified, only return entries with matching port tokens.
+       Entries without port tokens default to DEFAULTPORT."
+  (let ((rest list)
+        result)
+    (while list
+      (when (and (equal (cdr (assoc "machine" (car list))) machine)
+                 (equal (cdr (assoc "login" (car list))) account))
+        (push (car list) result))
+      (pop list))
+    (unless result
+      ;; No machine name matches, so we look for default entries.
+      (while rest
+        (when (assoc "default" (car rest))
+          (let ((elem (car rest)))
+            (setq elem (delete (assoc "default" elem) elem))
+            (push elem result)))
+        (pop rest)))
+    (when result
+      (setq result (nreverse result))
+      (if (not port)
+          (car result)
+        (while (and result
+                    (not (netrc-port-equal
+                          (or port defaultport "nntp")
+                          ;; when port is not given in the netrc file,
+                          ;; it should mean "any port"
+                          (or (netrc-get (car result) "port")
+                              defaultport port))))
+          (pop result))
+        (car result)))))
+
+(defun offlineimap-get-password (host account port)
+  (let* ((netrc (netrc-parse (expand-file-name "~/.authinfo.gpg")))
+         (hostentry (netrc-account-machine netrc host account port port)))
+    ;;(message "host: %s" hostentry)
+    (when hostentry (netrc-get hostentry "password"))))
+
 (provide 'init-gnus)
 
 ;;; init-gnus.el ends here
