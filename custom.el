73bdcd80b3d7379e5d18808000bfa8fe1e62a13d
Zhong Lizhi <zhonglizhi@8chedao.com>
2015-01-12 18:06:04 +0800

    重新配置java相关的内容

--- custom.el
+++ custom.el
@@ -16,25 +16,36 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eclim-eclipse-dirs '("~/programs/eclipse"))
- '(eclim-executable "~/programs/eclipse/eclim")
  '(bbdb-file "~/.bbdb.gpg")
  '(browse-url-browser-function (quote browse-url-firefox))
  '(canlock-password "90e0c564e3c4b436c584cc82b82e76914f7581f7")
  '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
- '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(custom-safe-themes
+   (quote
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(eclim-eclipse-dirs (quote ("~/programs/eclipse")))
+ '(eclim-executable "~/programs/eclipse/eclim")
  '(jde-ant-home "~/apache/ant")
  '(jde-ant-program "~/apache/ant/bin/ant")
  '(jde-build-function (quote prj-build))
  '(jde-global-classpath (quote ("~/java/jdk/" "build/main")))
- '(jde-sourcepath (quote ("~/java/jdk1.8.0_05/src.zip" "~/.ivy2/sources/*/*.jar" "~/workspace/*/*/src" "~/projects/*/src")))
+ '(jde-sourcepath
+   (quote
+    ("~/java/jdk1.8.0_05/src.zip" "~/.ivy2/sources/*/*.jar" "~/workspace/*/*/src" "~/projects/*/src")))
  '(large-file-warning-threshold 60000000)
  '(mm-tmp-directory "~/tmp/")
  '(org-agenda-files (quote ("~/org/2014" "~/org/2015")))
- '(org-export-latex-verbatim-wrap (quote ("\\begin{minted}[bgcolor=mintedbg]{text}
+ '(org-export-latex-verbatim-wrap
+   (quote
+    ("\\begin{minted}[bgcolor=mintedbg]{text}
 " . "\\end{minted}")))
  '(org-mobile-directory "~/Dropbox/Apps/MobileOrg")
- '(package-archives (quote (("melpa-stable" . "http://stable.melpa.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/") ("org" . "http://orgmode.org/elpa/") ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(package-archives
+   (quote
+    (("melpa-stable" . "http://stable.melpa.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/")
+     ("org" . "http://orgmode.org/elpa/")
+     ("gnu" . "http://elpa.gnu.org/packages/"))))
  '(semantic-idle-scheduler-idle-time 3600)
  '(semantic-java-dependency-system-include-path (quote ("~/java/jdk")))
  '(semanticdb-default-save-directory "~/tmp/semanticdb")
--- z-lisp/init-java.el
+++ z-lisp/init-java.el
@@ -1,86 +1,42 @@
-;
-
+;;; init-java.el -- configure java
 ;;; Commentary:
 ;;
 
 ;;; Code:
 ;; jde
 (setq debug-on-error t)
-(add-to-list 'load-path (expand-file-name "~/gnu/plugins/jdee/lisp"))
+;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/jdee/lisp"))
 ;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/cedet/semantic"))
 ;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/cedet/speedbar"))
-(add-to-list 'load-path (expand-file-name "~/gnu/plugins/elib"))
+;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/elib"))
 ;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/cedet/eieio"))
 ;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/cedet/cogre"))
 ;;(add-to-list 'load-path (expand-file-name "~/gnu/plugins/cedet/ede"))
 ;;(load-file "~/gnu/plugins/cedet/common/cedet.el")
 
 ;;(setq defer-loading-jde nil)
+
+(require 'eieio)
+
+;; EDE 工程管理负责编译构建
 (require 'ede)
 (require 'srecode)
 
-(require 'eieio)
 (global-ede-mode t)
-
-(setq defer-loading-jde t)
-(if defer-loading-jde
-    (progn
-      (autoload 'jde-mode "jde" "JDE mode." t)
-      (setq auto-mode-alist
-	    (append
-	     '(("\\.java\\'" . jde-mode))
-	     auto-mode-alist)))
-  (require 'jde))
-;;(require 'jde)
 
 ;; Sets the basic indentation for Java source files
 ;; to two spaces.
-(defun my-jde-mode-hook ()
-  (setq c-basic-offset 2))
-
-(add-hook 'java-mode-hook 'my-jde-mode-hook)
-
-;; Include the following only if you want to run
-;; bash as your shell.
-
-;; Setup Emacs to run bash as its primary shell.
-(setq shell-file-name "bash")
-(setq shell-command-switch "-c")
-(setq explicit-shell-file-name shell-file-name)
-(setenv "SHELL" shell-file-name)
-(setq explicit-sh-args '("-login" "-i"))
-(if (boundp 'w32-quote-process-args)
-    (setq w32-quote-process-args ?\")) ;; Include only for MS Windows.
+(setq c-basic-offset 2)
 
 ;;YaSnippet
-;;(add-to-list 'load-path "~/gnu/plugins/yasnippet")
 (require 'yasnippet)
-;(autoload 'yasnippet "yasnippet" "Select yasnippet" t)
 (yas-global-mode 1)
-
-;; auto complete
-;(add-to-list 'load-path "~/gnu/plugins/auto-complete") ;; This may not be appeared if you have already added.
-;;(add-to-list 'ac-dictionary-directories "~/emacs-24.2/plugin/auto-complete/ac-dict")
-;(require 'auto-complete-config)
-;(autoload 'auto-complete-config "auto-complete-config" "Select auto-complete-config" t)
-;(ac-config-default)
-
-;; auto java complete
-(add-to-list 'load-path "~/gnu/plugins/ajc-java-complete/")
-(require 'ajc-java-complete-config)
-;(autoload 'ajc-java-complete-config "ajc-java-complete-config" "Select ajc-java-complete-config")
-(add-hook 'java-mode-hook 'ajc-java-complete-mode)
-(add-hook 'find-file-hook 'ajc-4-jsp-find-file-hook)
 
 ;; junit
 (require 'jde-junit)
 (require 'jde-ant)
 ;;(autoload 'jde-junit "jde-junit" "Select jde-junit" t)
 ;;(autoload 'jde-ant "jde-ant" "Select jde-ant" t)
-
-;; jde contrib: decompile
-;;(add-to-list 'load-path "~/gnu/plugins/jdee/contrib/")
-;;(require 'decompile)
 
 ;; my script
 (require 'jde-parse)
@@ -174,24 +130,24 @@
   "Compile current java file when you save the file."
   (if (prj-is-java-sourcefile)
       (let* ((working-dir (file-name-directory
-                          (jde-find-project-file (file-truename "."))))
+                           (jde-find-project-file (file-truename "."))))
              (abslute-buildfile-path (concat working-dir jde-ant-buildfile)))
         (if (eq nil (search "/test/" (file-truename "."))) ; then
             (jde-ant-build abslute-buildfile-path
-                         "common.compile"
-                         (concat "-Dsrc.dir.includes=**/" (buffer-name)))
+                           "common.compile"
+                           (concat "-Dsrc.dir.includes=**/" (buffer-name)))
           ;; else
           (jde-ant-build abslute-buildfile-path
-                       "common.compile-test"
-                       (concat "-Dsrc.test.includes=**/" (buffer-name)))
-        ))
-      )
+                         "common.compile-test"
+                         (concat "-Dsrc.test.includes=**/" (buffer-name)))
+          ))
+    )
   )
 (add-hook 'after-save-hook 'prj-after-save-hook)
 
 (custom-set-variables
  '(jde-build-function (quote prj-build)))
-(provide 'init-jdee)
+(provide 'init-java)
 
 ;;; init-jdee.el ends here
 
